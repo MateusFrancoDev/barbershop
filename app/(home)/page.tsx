d2d3604205e5f1f6fd15f7c024dc9c3ef699ec8d
Mateus Franco <112667339+MateusFrancoDev@users.noreply.github.com>
2025-09-02 18:42:18 -0300

    feat: mensagem de início

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,18 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="p-5 pt-5">
+        <h2 className="text-lg font-bold">Olá, Mateus</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE', 'dd' de' MMMM'", { locale: ptBR })}
+        </p>
+      </div>
     </div>
   );
 }
