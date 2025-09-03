83b6bbaea12ff6ccb3169f7824f02c88c96d069e
Mateus Franco <112667339+MateusFrancoDev@users.noreply.github.com>
2025-09-03 16:55:50 -0300

    feat: Pagina de informações da barbearia

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/app/_components/ui/button";
+import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import BarbershopInfo from "./_components/barbershop-info";
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  };
+}
+
+const BarbershopDetailsPage = async ({
+  params,
+}: BarbershopDetailsPageProps) => {
+  if (!params.id) {
+    // TODO: redirecionar para home
+    return null;
+  }
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+  if (!barbershop) {
+    return null;
+  }
+  return <BarbershopInfo barbershop={barbershop} />;
+};
+
+export default BarbershopDetailsPage;
